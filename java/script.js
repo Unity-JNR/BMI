01f342ce22fa1840aab58268736742ae7cbb1140
Unity-JNR <unitydemas12@gmail.com>
2023-11-13 15:25:29 +0200

    BMI

---
+++ java/script.js
@@ -0,0 +1,17 @@
+let btn = document.querySelector('[data-weight]');
+let weight = document.getElementById('weight').value;
+let height = document.getElementById('height').value;
+
+btn.addEventListener('click',function(){
+    let weight = document.getElementById('weight').value;
+    let height = document.getElementById('height').value;
+
+     BMI(weight,height);
+    
+})
+
+
+function BMI(w,h){
+let sum = w/h;
+document.getElementById('BMI').value = sum.toFixed(2)
+}
