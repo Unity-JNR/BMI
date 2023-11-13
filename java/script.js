9c2c67fae12a23172e4bc56cae429b073de74be7
UnityDemas <98840485+UnityDemas@users.noreply.github.com>
2023-11-13 20:25:32 +0200

    added range

--- java/script.js
+++ java/script.js
@@ -13,5 +13,23 @@ btn.addEventListener('click',function(){
 
 function BMI(w,h){
 let sum = w/(h*h);
-document.getElementById('BMI').value = sum.toFixed(2)
+document.getElementById('BMI').value = sum.toFixed(2);
+let range = document.querySelector('[data-range]');
+if(sum <18.5 ){
+    
+    range.innerText = "underweight"
+} else if (sum >=18.5 && sum <= 24.9){
+    range.innerText ="perfectly healthy"
+} else if(sum >25 && sum <=29.9){
+    range.innerText =" overweight"
+
+} else if(sum >= 30 && sum<39.9){
+    range.innerText ="obese"
+    
+ } else if(sum>40){
+    range.innerText ="severe obese"
+    
+ }
+
+
 }
